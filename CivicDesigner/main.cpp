8a751d2d0b720caf66f92aad329f653a84f3a841
Breyden Monyemoratho <mnybre002@myuct.ac.za>
2019-08-05 20:58:03 +0200

    Initial Commit

---
+++ CivicDesigner/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CivicDesigner
+//
+//  Created by Breyden Monyemoratho on 2019/08/05.
+//  Copyright © 2019 Breyden Monyemoratho. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
